4503af48ee46fdd882403ea7023b290fe2b50d30
Alex <AN270909@falmouth.ac.uk>
2022-04-06 21:36:01 +0100

    Alex scene updates

--- Assets/GameStuff/Scripts/EnemyAI/EnemySpawner.cs
+++ Assets/GameStuff/Scripts/EnemyAI/EnemySpawner.cs
@@ -9,6 +9,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] GameObject[] listOfenemy;
     [SerializeField] List<PatrolList> patrolLists = new List<PatrolList>();
     public GameObject EnemyPrefab;
+    public GameObject EnemyRangedPrefab;
 
     // Start is called before the first frame update
     void Start()
